b94ddd0a159428b991eae50d61c81e98a8a5a865
Erdem Ege Eroğlu <erdemegeeroglu@Erdem-MacBook-Pro.local>
2025-02-25 19:44:35 +0300

    engine version env2

--- next.config.js
+++ next.config.js
@@ -10,7 +10,41 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  // Webpack yapılandırması için ek ayarlar
+  webpack: (config, { isServer }) => {
+    // Chunk boyutunu küçültmek için yapılandırma
+    config.optimization.splitChunks = {
+      chunks: 'all',
+      maxInitialRequests: 25,
+      minSize: 20000,
+      maxSize: 24000000, // 24MB (Cloudflare limitinin altında)
+      cacheGroups: {
+        default: false,
+        vendors: false,
+        commons: {
+          name: 'commons',
+          chunks: 'all',
+          minChunks: 2,
+          reuseExistingChunk: true,
+        },
+        lib: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+            return `npm.${packageName.replace('@', '')}`;
+          },
+          chunks: 'all',
+          minChunks: 1,
+          reuseExistingChunk: true,
+        },
+      },
+    };
+
+    return config;
+  },
 }
 
 module.exports = nextConfig 
